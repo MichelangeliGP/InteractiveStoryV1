e776997a315a7b2eda8dc2b923b5603ec353e743
Michelangeli Gayon <michelangeligayon@Michelangelis-MacBook-Pro.local>
2016-10-21 22:33:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/michelangeligayon/interactivestory/model/Choice.java
@@ -0,0 +1,32 @@
+package com.example.michelangeligayon.interactivestory.model;
+
+/**
+ * Created by michelangeligayon on 20/10/2016.
+ */
+
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
